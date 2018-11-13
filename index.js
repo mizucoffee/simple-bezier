5a24221124eaf83368760fcf2b7ce7be8750cd8e
Ritsuki Goto <develop@mizucoffee.net>
2018-11-14 00:33:24 +0900

    [add] param check

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 module.exports = (points, fineness) => {
+
+  if(points.length < 3) return []
+  if(fineness <= 0) return []
+  if(points.some(p => p.length < 2 || !Number.isFinite(p[0]) || !Number.isFinite(p[1]))) return []
+
   let lines = []
   for (let i = 0; i < points.length - 1; i++) lines.push(lineSplit(points[i][0], points[i][1],points[i+1][0], points[i+1][1], fineness))
 
